5fcb502c3b6668420aa0412591a737dbed404143
Luke Murphey <Luke.Murphey@gmail.com>
2020-03-18 12:17:55 -0700

    Updating the about page

--- companion-website/src/About.js
+++ companion-website/src/About.js
@@ -12,27 +12,48 @@ import {
             About
             </Header>
 
+            I wrote this material because I couldn't find content that quite fit with what I wanted.
+            Here are some things I found that I didn't think was ideal:
+
+            <Header as='h3' dividing>1. Was too broad</Header>
+            Some of the evidences content I found cast a very wide net into a large number of topics that I think spread people's knowledge too thin.
+
+            <Header as='h3' dividing>2. Didn't translate to a class environment</Header>
+            Many apologetics books do not translate well to a class environment.
+            They were written for readers to read but not for teachers to teach.
+            Some of the books had a study guide but I found them oftentimes confusing.
+            It was clear that the study guide was made after the book as an afterthought.
+
+            <Header as='h3' dividing>3. Didn't expose Christian's to some of the latest research</Header>
+            Great gains have been made in recent years in New Testament studies in several areas.
+            For example, recent research has provided more reasons to see the Gospels as the product of eyewitness testimony.
+            Too often, Christian's are unaware of these recent developments.
+
             <Header as='h2' dividing>
-            Why another apologetics book?
+            How is this material different?
             </Header>
 
+            Here are some things that makes this material different:
+
+            <Header as='h3' dividing>1. A focus on Jesus’ resurrection</Header>
+
             <ol>
-                <li>
-                    Many apologetics books do not translate well to a class environment.
-                    They are written for readers to read but not for teachers to teach.
-                </li>
-                <li>
-                    Some evidences books focus on areas that Christian's don't typically have a background in (for example, in scientific disciplines).
-                    Doing so means that Christian's often are not well equipped to use the content or use it with too little background to use it well.
-                    This curiculum focuses on expanding the Christian's strong point: the New Testament.
-                    In general, Christian's know more about the New Testmant than the general populace and thus are better equipped to use the knowledge.
-                </li>
-                <li>
-                    Great gains have been made in recent years in New Testament studies in several areas.
-                    For example, recent research has provided more reasons to see the Gospels as the product of eyewitness testimony.
-                    Too often, Christian's are unaware of these recent developments.
-                </li>
+                <li>Its Biblical: Paul says so in 1 Corinthians 15</li>
+                <li>It prevents you from getting overloaded because it focuses on a small set of main questions.</li>
+                <li>It lets you focus on your strength (the New Testament) as opposed to asking you to take on a skill you have little background in.</li>
+                <li>It allows you to become more knowledgeable in the Gospel accounts (nice side benefit to the main apologetic reason)</li>
             </ol>
+
+            <Header as='h3' dividing>2. Materials for assisting the teacher</Header>
+
+            This material is based upon the assumption that the content will be taught in a class environment. As such, it provides:
+
+            <ol>
+                <li>Briefing statements to help the teacher know </li>
+                <li>Discussion notes for the teacher to help them run the class</li>
+                <li>Slides including visualizations, quotes and pictures to make the content more understandable</li>
+                <li>References to related books and videos to enable the teacher to be prepared</li>
+            </ol>            
 
         </Container>
       )
