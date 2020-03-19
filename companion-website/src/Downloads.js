ea11820a518d9b20e2bdf66644dbfedd0a35ed09
Luke Murphey <Luke.Murphey@gmail.com>
2020-03-19 12:15:00 -0700

    Adding links and downloads

--- companion-website/src/Downloads.js
+++ companion-website/src/Downloads.js
@@ -2,15 +2,41 @@ import React, { Component } from 'react'
 import {
   Container,
   Header,
+  Table,
+  Popup,
 } from 'semantic-ui-react'
+import downloads from './downloads.json'
 
   class Downloads extends Component {
+
+    downloadFile(file) {
+      window.open(`files\\${file}`);
+    }
+
     render() {
       return (
         <Container text>
         <Header as='h1' dividing>
           Downloads
         </Header>
+
+        <Table celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Title</Table.HeaderCell>
+              <Table.HeaderCell>Links</Table.HeaderCell>
+            </Table.Row>
+         </Table.Header>
+
+         <Table.Body>
+          {downloads.map((item, index) => (
+            <Table.Row key={index}>
+              <Table.Cell><Popup content={item.description} trigger={<div>{item.title}</div>} /></Table.Cell>
+              <Table.Cell><a href="#" onClick={() => this.downloadFile(item.file)}>Download</a></Table.Cell>
+            </Table.Row>
+          ))}
+         </Table.Body>
+        </Table>
   
         </Container>
       )
