0f1d1c9ea471000f0371393cf986b93d5adf27b9
Luke Murphey <Luke.Murphey@gmail.com>
2020-03-19 12:14:40 -0700

    Updating the about page

--- companion-website/src/About.js
+++ companion-website/src/About.js
@@ -13,48 +13,38 @@ import {
             </Header>
 
             I wrote this material because I couldn't find content that quite fit with what I wanted.
-            Here are some things I found that I didn't think was ideal:
+            Here are some things that makes this material a little different:
 
-            <Header as='h3' dividing>1. Was too broad</Header>
-            Some of the evidences content I found cast a very wide net into a large number of topics that I think spread people's knowledge too thin.
+            <Header as='h3' dividing>1. Focus on a defense the New Testament</Header>
+            Some of the evidences content casts a very wide net into a large number of topics that spread people's knowledge too thin (such as Entomology, Cosmology, etc.).
+            This content focuses on the New Testament since this is the thing that Christian's most likely know well.
+            Specifically, this content focuses on a defense of Jesus' resurrection and the Gospel accounts because:
 
-            <Header as='h3' dividing>2. Didn't translate to a class environment</Header>
+            <ol>
+                <li>Christianity rises or falls on the resurrection: Paul says so in 1 Corinthians 15</li>
+                <li>This prevents Christians from being overloaded because it focuses the truth of Christianity on a smaller set of questions.</li>
+                <li>This lets Christians focus on their strength (the New Testament) as opposed to asking you to take on a skill that they have little background in.</li>
+                <li>It allows the class to become more knowledgeable in the Gospel accounts (nice side benefit to the main apologetic reason)</li>
+            </ol>
+
+            <Header as='h3' dividing>2. Material focused on supporting a class environment</Header>
             Many apologetics books do not translate well to a class environment.
             They were written for readers to read but not for teachers to teach.
-            Some of the books had a study guide but I found them oftentimes confusing.
-            It was clear that the study guide was made after the book as an afterthought.
+            Some had a study guide but it was clear that they were made as an afterthought.
+
+            As such, it provides:
+
+            <ol>
+                <li>Briefing statements to help the teacher guide the class achieve the goals of each session</li>
+                <li>Discussion notes for the teacher to help them manage the discussion</li>
+                <li>Slides including visualizations, quotes and pictures to make the content more understandable and impactful</li>
+                <li>References to related books and videos to enable the teacher to be prepared</li>
+            </ol>
 
             <Header as='h3' dividing>3. Didn't expose Christian's to some of the latest research</Header>
             Great gains have been made in recent years in New Testament studies in several areas.
             For example, recent research has provided more reasons to see the Gospels as the product of eyewitness testimony.
-            Too often, Christian's are unaware of these recent developments.
-
-            <Header as='h2' dividing>
-            How is this material different?
-            </Header>
-
-            Here are some things that makes this material different:
-
-            <Header as='h3' dividing>1. A focus on Jesus’ resurrection</Header>
-
-            <ol>
-                <li>Its Biblical: Paul says so in 1 Corinthians 15</li>
-                <li>It prevents you from getting overloaded because it focuses on a small set of main questions.</li>
-                <li>It lets you focus on your strength (the New Testament) as opposed to asking you to take on a skill you have little background in.</li>
-                <li>It allows you to become more knowledgeable in the Gospel accounts (nice side benefit to the main apologetic reason)</li>
-            </ol>
-
-            <Header as='h3' dividing>2. Materials for assisting the teacher</Header>
-
-            This material is based upon the assumption that the content will be taught in a class environment. As such, it provides:
-
-            <ol>
-                <li>Briefing statements to help the teacher know </li>
-                <li>Discussion notes for the teacher to help them run the class</li>
-                <li>Slides including visualizations, quotes and pictures to make the content more understandable</li>
-                <li>References to related books and videos to enable the teacher to be prepared</li>
-            </ol>            
-
+            Too often, Christian's are unaware of these recent developments.           
         </Container>
       )
     }
